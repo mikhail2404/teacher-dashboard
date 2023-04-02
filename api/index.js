15218b30d59b0b1049686e376c3b35740cacf98d
Mikhail <mike@drupfan.com>
2023-04-02 13:19:06 +0300

    Daployment requirements

--- api/index.js
+++ api/index.js
@@ -1,12 +1,33 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 const cors = require('cors');
 dotenv.config();
 
 app.use(cors());
-app.get('/api/dashboard', (req, res) => {
-    res.json('hello world 2 '+Date.now());
+const DashboardModel = require("./models/Dashboard");
+const StudentModel = require("./models/Student");
+
+mongoose.connect(process.env.DB_CONNECT)
+.then(()=> console.log("Database connected"))
+.catch(err => console.log(err))
+app.get('/api/dashboard', async (req, res)=>{
+    try {
+        const dashboardData = await DashboardModel.find();
+        res.json(dashboardData);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+})
+
+app.get('/api/students', async (req, res) => {
+    try {
+        const students = await StudentModel.find();
+        res.json(students);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
 });
 
 if (process.env.API_PORT) {
