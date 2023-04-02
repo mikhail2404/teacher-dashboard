c6b9f09bf61a04599065382340dbe04d11d2a7bb
Mikhail <mike@drupfan.com>
2023-04-02 13:03:59 +0300

    Chaned api file

--- api/index.js
+++ api/index.js
@@ -1,50 +1,16 @@
 const express = require('express');
-// const mongoose = require('mongoose');
-const dotenv = require('dotenv').config();
-const cors = require('cors');
-
 const app = express();
-//use express.json() to get data into json format
-app.use(express.json());
-//Port 
+const dotenv = require('dotenv');
+const cors = require('cors');
+dotenv.config();
 
-//use cors
 app.use(cors());
-
-//import routes
-const DashboardModel = require("./models/Dashboard");
-const StudentModel = require("./models/Student");
-
-
-//connect to mongodb ..
-// mongoose.connect(process.env.DB_CONNECT)
-// .then(()=> console.log("Database connected"))
-// .catch(err => console.log(err))
-
-app.get('/api/dashboard', async (req, res)=>{
+app.get('/api/dashboard', (req, res) => {
     res.json('hello world 2 '+Date.now());
-
-    // try {
-    //     const dashboardData = await DashboardModel.find();
-    //     res.json(dashboardData);
-    // } catch (err) {
-    //     res.status(500).json({ message: err.message });
-    // }
-})
-
-// app.get('/api/students', async (req, res) => {
-//     try {
-//         const students = await StudentModel.find();
-//         res.json(students);
-//     } catch (err) {
-//         res.status(500).json({ message: err.message });
-//     }
-// });
-
+});
 
 if (process.env.API_PORT) {
     app.listen(process.env.API_PORT);
 }
 
-
-//connect to api
+module.exports = app;
