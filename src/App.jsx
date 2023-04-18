2e31a6e7cece44b9ad5e5cecbbf91e912409a6c0
Mikhail <mike@drupfan.com>
2023-04-18 18:08:22 +0300

    Transformed app to class based

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 
-
-function App() {
-    return (
-        <div id="dashboard">
-            <BrowserRouter>
-                <Layout />
-            </BrowserRouter>
-        </div>
-    );
+class App extends Component {
+    render() {
+        return (
+            <div id="dashboard">
+                <BrowserRouter>
+                    <Layout />
+                </BrowserRouter>
+            </div>
+        );
+    }
 }
 
 export default App;
